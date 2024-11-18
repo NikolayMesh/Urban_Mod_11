30a9afb2dc1c4a1a5211dc7e846c32183213d096
NikolayMesh <partizan.91@bk.ru>
2024-11-18 20:35:58 +0300

    add

---
+++ module_11_2.py
@@ -0,0 +1,25 @@
+
+def introspection_info(obj):
+    """Возвращает словарь с информацией об объекте."""
+    # Сбор информации об объекте
+    info = {
+        'type': str(type(obj)).replace("<class '", "").replace("'>", ""),  # Убираем лишние символы
+        'attributes': dir(obj),  # Получаем все атрибуты и методы
+        'methods': [method for method in dir(obj) if callable(getattr(obj, method))],  # Фильтруем только методы
+        'module': obj.__module__ if hasattr(obj, '__module__') else None,  # Получаем модуль объекта, если он есть
+    }
+
+    return info
+
+# Пример использования:
+number_info = introspection_info(42)
+print('42', number_info)
+
+list_info = introspection_info([1, 2, 3])
+print('[1, 2, 3]',list_info)
+
+dict_info = introspection_info({'a': 1, 'b': 2})
+print("{'a': 1, 'b': 2}",dict_info)
+
+string_info = introspection_info("Hello")
+print('"Hello"',string_info)
